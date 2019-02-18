8d045b2b1ab4a0df79ec4f8d431cd777fedeb2b4
beztormoza <beztormoza@ukr.net>
2019-02-18 23:58:33 +0200

    fixed bugs + changed logic of accepting to internship

--- src/main/Application.java
+++ src/main/Application.java
@@ -8,25 +8,24 @@ public class Application {
     public static void main(String[] args) {
 
         University universityCHDTU = new University("CHDTU");
-        universityCHDTU.addStudent(new Student("Alex", 12));
-        universityCHDTU.addStudent(new Student("Den", 14));
-        universityCHDTU.addStudent(new Student("Bob", 50));
+        universityCHDTU.addStudents(new Student("Alex", 42));
+        universityCHDTU.addStudents(new Student("Bob", 50));
+
         Student Alice = new Student("Alice", 76);
         Student Jack = new Student("Jack", 10);
-        Jack.setKnowledgeLevel(80);
-        universityCHDTU.addStudent(Alice);
-        universityCHDTU.addStudent(Jack);
+        Jack.setKnowledgeLevel(90);
+        Student John = new Student("John");
+        John.setKnowledgeLevel(1000);                // checking for min/max (will show warning message)
+        universityCHDTU.addStudents(Alice, Jack, John, new Student("Den"));
+        System.out.println("");
         System.out.println(universityCHDTU.getName() + "'s list: ");
         universityCHDTU.showAllStudentsInfo();
 
         Internship interlink = new Internship("Interlink");
         interlink.inviteNewStudents(universityCHDTU.getStudents());
+        System.out.println("");
         System.out.println("Internship students list: ");
-        interlink.showAllStudentsInfo();
-
-        interlink.setMinKnowledgeLevel(10);
-        interlink.inviteNewStudents(universityCHDTU.getStudents());
-        System.out.println("Internship students list 2: ");
+        interlink.addStudents(new Student("Grand"));
         interlink.showAllStudentsInfo();
 
     }
